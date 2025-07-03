f7c68ee39283af44c9c67aceee448ab597345bf0
xiak27 <x33828052@gmail.com>
2025-07-03 17:55:55 +0800

    更新 白.lua

--- 白.lua
+++ 白.lua
@@ -1,7 +1,7 @@
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
-if player.Name == "666" then
+if player.Name == "3146X8" then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiak27/637/refs/heads/main/small%20empty%20script.lua"))()
 else
     player:Kick("有白名单吗就执行")
