78c9b4a2c0ff153c688f1c934a0947203e8154d8
xiak27 <x33828052@gmail.com>
2025-08-12 12:31:09 +0800

    Create X全屏提示.lua

---
+++ X全屏提示.lua
@@ -0,0 +1,14 @@
+local msg = Instance.new("Message",workspace)
+msg.Text = "欢迎使用小空脚本"
+wait(1.8)
+msg:Destroy()
+
+msg = Instance.new("Message",workspace)
+msg.Text = "正在加载中......"
+wait(1.8)
+msg:Destroy()
+
+msg = Instance.new("Message",workspace)
+msg.Text = "加载成功，即将执行脚本"
+wait(1.8)
+msg:Destroy()
